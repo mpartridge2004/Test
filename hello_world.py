c91e6f29ebac130291f8225d6fee92dce6bf134d
Michael Partridge <121437468+mpartridge2004@users.noreply.github.com>
2023-06-13 20:50:38 -0400

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,11 @@
 import streamlit as st
 
 st.write("""
-# my first app
-Hell **world! This sucks**
+# My First App
+Hello **world! This sucks**
 """)
 
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # Do something with the file
+    st.write("File has been uploaded!")
